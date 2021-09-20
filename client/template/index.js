fddfab23494c7610f4bf0888747694e0bf8e9ee0
brittney rawlings <bn.rawlings@gmail.com>
2021-09-20 13:08:40 -0500

    html files created

--- client/template/index.js
+++ client/template/index.js
@@ -1,1 +1,24 @@
 console.log("CONNECTED!")
+//CREATING A NEW USER
+const signUpbtn = document.querySelector(".signUp");
+const createNewUser = async () => {
+    const url = "";
+    const newUserName = document.querySelector(".username").value;
+    const newUser = {
+        //columnName User Name
+        //columnName Password
+        //columnName First Name
+        //columnName Last Name
+        //columnName Email 
+    };
+    const createUser = await fetch (url, {
+        method: "POST",
+        mode: "cors",
+        headers: {
+            "Content-type": "application/json; charset=UTF-8",
+        },
+        body: JSON.stringify(newUser),
+    });
+    console.log(createUser)
+};
+signUpbtn.addEventListener("click", () => createNewUser())
